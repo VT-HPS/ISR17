cbb4174c69f13e6a053b5a08f7811739750ddd60
Syenna Graham <syenna@MacBook-Air.local>
2023-05-16 17:47:14 -0400

    Changed RPM to Speed on GUI and Fixed Heading

--- gui.py
+++ gui.py
@@ -5,28 +5,30 @@ import time
 import serial
 from tkinter import *
 from tkinter import ttk
+import tkinter as tk
 from math import sqrt, pow
 from numpy import interp
 import threading
 import random
+from collections import deque
 
 #USB = '/dev/ttyACM0'        #raspberry pi - check which usb port the arduino is hooked to
-USB = '/dev/cu.usbmodem11301' #testing computer - check which usb port the arduino is hooked to
+USB = '/dev/cu.usbmodem11201' #testing computer - check which usb port the arduino is hooked to
 print("hit initial")
 gyro_list = [0,0]
 depth = 0
-psv = 0
-ps_value = 0
+speed = 0
+depth_value = 0
 rpm_value = 0 
 rpm_graphic_coord = 0
 depth_graphic_coord = 0
-deptg_indicator = 0
+depth_indicator = 0
 
 
 ######### CREATE DATALOGGING FILE ###################################################################################
 
 home_dir = os.path.expanduser('~') # Get the path to the user's home directory
-file_path = os.path.join(home_dir, 'ISR17/ISR17/serial_list_data.txt') # Create a file path in the home directory
+file_path = os.path.join(home_dir, 'ISR17/serial_list_data.txt') # Create a file path in the home directory
 print(file_path)
 
 ######### CONNECT WITH ARDUINO ######################################################################################
@@ -42,7 +44,7 @@ def read_arduino():
 # MINOR DISPLAY SETUP ITEMS
 root = Tk()  # initialize root variable
 root.geometry("800x480")  # root sized to hdmi monitor
-root.title('Nautilus HUD')
+root.title('Kraken HUD')
 
 # STYLE CONFIGURATION
 root.style = ttk.Style(root)
@@ -61,7 +63,7 @@ WIDTH = 200
 RADIUS = 30
 TAG = "cir"
 # labels
-heading_label = ttk.Label(root, text='HEADING', style='title.TLabel').grid(column=0, row=0, sticky='n')
+heading_label = ttk.Label(root, text='HEADING', style='title.TLabel', font=("Times", 20, "bold")).grid(column=0, row=0, sticky='n')
 heading_up = ttk.Label(root, text='UP', style='.TLabel').grid(column=0, row=1, sticky='n')
 heading_port = ttk.Label(root, text='PORT', style='.TLabel').grid(column=0, row=1, sticky='w', padx=100)
 heading_starboard = ttk.Label(root, text='STARBOARD', style='.TLabel').grid(column=0, row=1, sticky='e', padx=40)
@@ -76,35 +78,35 @@ heading_canvas.grid(column=0, row=1)
 
 # DEPTH DISPLAY SETUP
 # labels
-depth_label = ttk.Label(root, text='DEPTH', style='title.TLabel').grid(column=1, row=0, sticky='n')
+depth_label = ttk.Label(root, text='DEPTH', style='title.TLabel', font=("Times", 25, "bold")).grid(column=1, row=0, sticky='n')
 depth_value_label = ttk.Label(root, text="0", style='.TLabel')
-depth_value_label.place(x=650, y=45, anchor='n')
+depth_value_label.place(x=600, y=45, anchor='n')
 # canvas items
 depth_canvas = Canvas(root, height=400, width=100)
 depth_canvas.create_rectangle(3, 400, 100, 3, width='3')
-depth_canvas.grid(column=1, row=1, rowspan=2)
+depth_canvas.grid(column=1, row=1, rowspan=3)
 # (x0, y0, x1, y1) = (over 3, down 400, over 100, go down to depth #)
 depth_bar = depth_canvas.create_rectangle(3, 400, 100, depth, fill='#FFCC00')
 
 
-# RPM DISPlAY SETUP
-#labels
-RPM_label = ttk.Label(root, text='RPM', style='title.TLabel').grid(column=0, row=2, sticky="nw")
-RPM_value_label = ttk.Label(root, text="0", style='.TLabel')
-RPM_value_label.place(x=20, y=410, anchor='w')
-# canvas items
-RPM_canvas = Canvas(root, height=100, width=550)
-RPM_canvas.create_rectangle(550, 3, 3, 100, width='3')
-RPM_bar = RPM_canvas.create_rectangle(rpm_value, 3, 3, 100, fill='#FFCC00')
-RPM_canvas.grid(column=0, row=2)
+# SPEED DISPLAY SETUP
+frame_speed = ttk.Frame(root, padding=10, relief="solid", borderwidth=1)
+frame_speed.grid(column=0, row=3, sticky='w', padx=(20, 0), pady=(20, 50))
+frame_speed.configure(style="White.TFrame")
+style = ttk.Style()
+style.configure("White.TFrame", background="white")
+label_title = ttk.Label(frame_speed, text="SPEED: ", font=("Times", 25, "bold"))
+label_title.grid(column=0, row=0, sticky='w')
+label_result = ttk.Label(frame_speed, text="", font=("Times", 60))
+label_result.grid(column=1, row=0, sticky='w', padx=(50, 0))
 
 
 # change color of circle on display according to value
 def get_circle_color(x, y, radius):
     #check if coordinate is with in radius of origin
-    if  radius > int(sqrt( pow(abs(x-(WIDTH/2)), 2) + pow(abs(y-(HEIGHT/2)), 2))): 
+    if radius/2 > int(sqrt( pow(abs(x - (WIDTH/2)), 2) + pow(abs(y - (HEIGHT/2)), 2))): 
         return 'green'
-    elif radius * 2 < int(sqrt( pow(abs(x-(WIDTH/2)), 2) + pow(abs(y-(HEIGHT/2)), 2))): 
+    elif radius * 1.25 < int(sqrt( pow(abs(x - (WIDTH/2)), 2) + pow(abs(y - (HEIGHT/2)), 2))): 
         return 'red'
     else:
         return 'yellow'
@@ -127,27 +129,42 @@ def delete_circle(canvasName, tag):
 ######### INTERPRET PRESSURE SENSOR ######################################################################################
 
 # convert pressure sensor voltage to coordinates for canvas display
-def convert_volts_to_coord(psv_data):
-    #print('psv_data = ', psv_data)
-    depth_feet = round(psv_data/12)
-    print('depth_feet ', depth_feet)
+def convert_depth_to_coord(depth_value):
+    depth_feet = round(depth_value/12)
+    #print('depth_feet ', depth_feet)
     if depth_feet >= 30:
         return 3
     elif depth_feet <= 0:
         return 400
     else:
-        # print('psv_data ', psv_data)
-        # change these voltages for the pool voltages 0(0.5V) - 1023(4.5V)
         depth_indicator = interp(depth_feet,[0,30],[400,3])
     return int(depth_indicator)
 
 
 # calculate depth based on pressure 
-def calculate_depth(psv_data):
-    depth_feet = round(psv_data/12)
-    print("Depth Value (m) = ", depth_feet)
+def calculate_depth(depth_value):
+    depth_feet = round(depth_value/12)
+    #print("Depth Value (m) = ", depth_feet)
     return depth_feet
 
+
+# get speed value    
+def display_speed(insideSensorValue, outsideSensorValue):  
+    fluidDensity = 1000
+    sub_radius = 0.6604 # meters diameter of nautilis
+    flowArea = 3.14 * pow(sub_radius,2)
+
+    # Apply offset and scale to sensor readings
+    insidePressure = fluidDensity * 9.81 * insideSensorValue
+    outsidePressure = fluidDensity * 9.81 * outsideSensorValue
+
+    # Calculate the pressure difference
+    pressureDifference = abs(outsidePressure - insidePressure)
+
+    # Calculate the velocity
+    speed = round(sqrt((2 * pressureDifference) / (fluidDensity * flowArea)),1)
+    label_result.config(text="{} m/s".format(speed))
+    return speed
 
 
 ######### INTERPRET RPM SENSOR ######################################################################################
@@ -166,29 +183,24 @@ def convert_rpms_to_coord(data):
 
 ######### INTERPRET GYRO SENSOR ######################################################################################
 
-# map incoming gryo bytes to 1 - 9 
-def filter_gyro_coord(data): 
-    if data[2] > 90.0:
-        data[2] = 90
-    elif data[2] < -90:
-        data[2] = -90
-    else:
-        data[2] = data[2]
-
-    if data[3] > 90.0:
-        data[3] = 90
-    elif data[3] < -90:
-        data[3] = -90
-    else:
-        data[3] = data[3]
-    return data 
-
-
 # convert mapped gyro data to coordinates for display
-def convert_gyro_to_coord(data):
-    print('convert_gyro_to_coord(data): ', data)
-    y = interp(int(data[2]/10),[-9,9],[150,50])
-    x = interp(int(data[3]/10),[-9,9],[50,150])
+def convert_gyro_to_coord(pitch_value, yaw_value):
+    if pitch_value >= 15.0:
+        pitch_value = 90
+    elif pitch_value <= -15.0:
+        pitch_value = -90
+    else:
+        pitch_value = pitch_value
+
+    if yaw_value > 15.0:
+        yaw_value = 90
+    elif yaw_value < -15.0:
+        yaw_value = -90
+    else:
+        yaw_value = yaw_value
+
+    y = interp(int(pitch_value/10),[-9,9],[50,150])
+    x = interp(int(yaw_value/10),[-9,9],[50,150])
     gyro_list = [int(x), int(y)]
     return gyro_list
 
@@ -200,26 +212,28 @@ def gyro_dashboard():
     create_circle(gyro_list[0], gyro_list[1], RADIUS, heading_canvas, TAG)
 
 
-def rpm_dashboard():
-    rpm_graphic_coord = convert_rpms_to_coord(rpm_value)
-    RPM_canvas.coords(RPM_bar, rpm_graphic_coord, 3, 3, 100)
-    print('rpm_value = ', rpm_value)
-    RPM_value_label.config(text=str(rpm_value))
+# def rpm_dashboard():
+#     rpm_graphic_coord = convert_rpms_to_coord(rpm_value)
+#     RPM_canvas.coords(RPM_bar, rpm_graphic_coord, 3, 3, 100)
+#     #print('rpm_value = ', rpm_value)
+#     RPM_value_label.config(text=str(rpm_value))
     
 
-
 def depth_dashboard():
-    depth = calculate_depth(ps_value)
-    depth_graphic_coord = convert_volts_to_coord(ps_value)
+    depth = calculate_depth(depth_value)
+    depth_graphic_coord = convert_depth_to_coord(depth_value)
     depth_canvas.coords(depth_bar, 3, 400, 100, depth_graphic_coord)
-    depth_value_label.config(text=str(depth))
+    depth_value_label.config(text=str(depth), font=("Times", 25, "bold"))
+
+def speed_dashboard():
+    display_speed(insideSensorValue, outsideSensorValue)
 
 
 def update_gui():
     while True:
-        print('new update ---------------------------------------------------')
         gyro_dashboard()
-        rpm_dashboard()
+        speed_dashboard()
+        #rpm_dashboard()
         depth_dashboard()
         time.sleep(0.1)
 
@@ -232,10 +246,10 @@ def close_win():
 def get_random_xy_coord():
     global gyro_coord
     global rpm_value
-    global decode_rpms
     global depth_indicator
-    global decode_ps_voltage
     global ps_value
+    global insideSensorValue
+    global outsideSensorValue
     global rpm_graphic_coord
 
     # CREATE A FILE FOR OUTPUTING SERIAL DATA FOR DATALOGGING
@@ -247,13 +261,10 @@ def get_random_xy_coord():
             x = interp(int(data[1]/10),[-9,9],[50,150])
             gyro_coord = [int(x), int(y)]
 
-            rpm_value = random.randrange(0, 250)
-            #rpm_value = 100
-            print('rpm_value = ', rpm_value)
-            
+            rpm_value = random.randrange(0, 250)      
 
             ps_value = random.randrange(12, 360)
-            depth_indicator = convert_volts_to_coord(ps_value)
+            depth_indicator = convert_depth_to_coord(ps_value)
             serial_list = [depth_indicator, rpm_value, int(x), int(y)]
             print('SERIAL LIST ', str(serial_list))
             
@@ -265,35 +276,32 @@ def get_random_xy_coord():
 
 
 def read_sensor_data():
-    global ps_value
+    global insideSensorValue
+    global outsideSensorValue
     global rpm_value
+    global speed
     global depth_graphic_coord
     global rpm_graphic_coord
     global gyro_list
     serial_list = []
-    serial_list_backup = [0,0,0,0,0,0]
+    serial_list_backup = [0,0,0,0,0,0,0]
 
     # CREATE A FILE FOR OUTPUTING SERIAL DATA FOR DATALOGGING
     with open(file_path, 'w') as output_file:
         while True:
             serial_list = []
             data = read_arduino()
-            print('--------------------------------------------')
-            print('data: ', data)        
+            #print('--------------------------------------------')
+            #print('incoming serial data: ', data)        
             try: 
                 serial_string = (data.decode('ascii'))
             except:
                 serial_string = '' 
-            #serial_string = serial_string.replace('\r', '')
             serial_string = serial_string.strip()
-            print('serial string strip!!')
-            print(serial_string)
-            #serial_string = re.sub('[^\d,.-]|[.-](?=[^.]*[.])', '', serial_string) 
             serial_string_split = serial_string.split(",")
-            print('serial string split = ', serial_string_split)
+            #print('serial string split = ', serial_string_split)
 
             if (data == b'') or (re.match( r'^\.' or r'^\>' or '^\!', serial_string)) :
-                print('data2 ', data)
                 serial_list = serial_list_backup.copy()
             else:    
                 if "!" not in serial_string.split(","):
@@ -305,35 +313,43 @@ def read_sensor_data():
                                 serial_list.append(0)
                         else:
                             serial_list.append(0)
-                    print("Original Serial List: ", serial_list)
-                    if len(serial_list) != 6 and len(serial_list) != 5:
-                        serial_list = [0,0,0,0,0,0]
-                    print('serial data list: ', serial_list)
+                    
+                    if len(serial_list) != 7:
+                       serial_list = serial_list_backup.copy()
+                    
                     serial_list_backup = serial_list.copy()
-                    print('SERIAL LIST BU', serial_list_backup)
+                
                 else:
                     serial_list = serial_list_backup.copy()
-                    print('HIT BACK UP SERIAL LIST')
-                     
-                
-            filtered_gyro_values = filter_gyro_coord(serial_list) # make sure gyro data is between -90 and 90
-            print('serial list with filtered gyro values: ', filtered_gyro_values) 
-            print('')
+            
 
-            gyro_list = convert_gyro_to_coord(filtered_gyro_values)
-            print('gyro_list: ', gyro_list, '- mapped from [-9,9],[50,150] and [-9,9],[150,50]') 
-
-            ps_value = serial_list[0]
-            depth_graphic_coord = convert_volts_to_coord(ps_value)
-            print('ps_coord: ', depth_graphic_coord, ' - mapped from [0,1023] to [400,100]') 
-
-            rpm_value = serial_list[1]
+            insideSensorValue = serial_list[0]
+            depth_graphic_coord = convert_depth_to_coord(insideSensorValue)
+            
+            outsideSensorValue = serial_list[1]
+            
+            rpm_value = serial_list[2]
             rpm_graphic_coord = convert_rpms_to_coord(rpm_value)
-            print('rpm_graphic_coord: ', rpm_graphic_coord, '- mapped from [0,250] to [3,550]') 
-            print('SERIAL LIST = ', serial_list)
 
-            control_surface_degrees = serial_list[4:]
+            pitch_value = serial_list[3]
+            yaw_value = serial_list[4]
+            gyro_list = convert_gyro_to_coord(pitch_value, yaw_value)
+            
+            #control_surface_degrees = serial_list[5:]
             #print("Pitch degrees: ", control_surface_degrees[0], ", Yaw degrees: ", control_surface_degrees[1])
+
+            if (data == b'') or (re.match( r'^\.' or r'^\>' or '^\!', serial_string)):
+                #print('HIT BACK UP SERIAL LIST')
+                continue
+            else:
+                print('--------------------------------------------')
+                print('SERIAL LIST = ', serial_list) 
+                print('depth : ', serial_list[1], ' in') 
+                print('outsideSensorValue: ', outsideSensorValue, ' m/s')      
+                print('rpm: ', rpm_value)                 
+                print('Yaw value: ', pitch_value)
+                print('Pitch value: ', yaw_value)
+
 
             # OUTPUT SERIAL DATA FOR DL
             output_file.write(str(serial_list) + "\n")
@@ -350,6 +366,8 @@ if '__main__' == __name__:
     th.start()
     
     update_gui()
+
+    time.sleep(30)  # Pause for 3 seconds
 
     root.mainloop()  # run hud
     #test.mainloop()
